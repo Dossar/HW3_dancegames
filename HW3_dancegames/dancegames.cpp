690b39df88cf7775bc8f68c64e3209cafc0f99fa
Roy Van Liew <vanliewroy@gmail.com>
2014-02-12 19:11:34 -0500

    Getting set up

---
+++ HW3_dancegames/dancegames.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   dancegames.cpp
+ * Author: Roy Van Liew and Saqib Zahid
+ *
+ * Created on February 12, 2014, 7:04 PM
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+    return 0;
+}
+
